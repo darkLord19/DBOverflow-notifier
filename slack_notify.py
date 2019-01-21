971eae5622f125b661f0d23d54cdcc141b8537d6
Umang Parmar <umangjparmar@gmail.com>
2019-01-21 13:32:03 +0530

    Send message to slack incoming webhook

---
+++ slack_notify.py
@@ -0,0 +1,19 @@
+import requests, json
+from secrets import Constants
+
+headers = {
+    'Content-type': 'application/json',
+}
+
+data = {"text" : "Sent using python code!"}
+
+response = requests.post(
+    Constants.WEBHOOK, data=json.dumps(data),
+    headers={'Content-Type': 'application/json'},
+    verify=False
+)
+if response.status_code != 200:
+    raise ValueError(
+        'Request to slack returned an error %s, the response is:\n%s'
+        % (response.status_code, response.text)
+    )
