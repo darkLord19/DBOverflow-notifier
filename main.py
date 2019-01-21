d0b8fd86bd4cf0d88de9b3ef92be6d31f91e614c
Umang Parmar <umangjparmar@gmail.com>
2019-01-21 16:22:52 +0530

    Get list of tables which have auto increament fields which are about to run out

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from db_connection import *
 from slack_notify import *
 
+
+def about_to_run_out(current_field_max, field_maxvalue):
+    if (100 * current_field_max) / field_maxvalue >= 90:
+        return True
+    return False
+
+
 mydb = connect()
 tables_with_auto_incr = get_auto_icr_fld(mydb)
 
-print(tables_with_auto_incr)
+tables_having_fld_abt_run_out = []
+
+for table in tables_with_auto_incr:
+    if about_to_run_out(table.current_max, table.field_maxvalue):
+        tables_having_fld_abt_run_out.append(table)
