6e9499236bc549d71f8a4250c6866ac29dac14cc
Umang Parmar <umangjparmar@gmail.com>
2019-01-22 10:04:52 +0530

    Error handling

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ tables_with_auto_incr = get_auto_icr_fld(mydb)
 
 tables_having_fld_abt_run_out = []
 
-for table in tables_with_auto_incr:
-    if about_to_run_out(table.current_max, table.field_maxvalue):
-        tables_having_fld_abt_run_out.append(table)
-
-notify(tables_having_fld_abt_run_out)
+if len(tables_with_auto_incr) > 0:
+    for table in tables_with_auto_incr:
+        if about_to_run_out(table.current_max, table.field_maxvalue):
+            tables_having_fld_abt_run_out.append(table)
+    if len(tables_having_fld_abt_run_out) > 0:
+        notify(tables_having_fld_abt_run_out)
+    else:
+        print("Your Database is safe for now.")
+else:
+    print("Your Database doesn't have any auto incrimenting field.")
