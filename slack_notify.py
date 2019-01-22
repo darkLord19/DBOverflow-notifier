64095c6b7e7e38150aa478527b18eab447fe36ac
Umang Parmar <umangjparmar@gmail.com>
2019-01-22 09:59:50 +0530

    Send message for all tables and not only last

--- slack_notify.py
+++ slack_notify.py
@@ -5,6 +5,7 @@ from secrets import Constants
 def notify(table_list):
     headers = {"Content-type": "application/json"}
 
+    # Nofify for every table found
     for table in table_list:
         msg = "***ATTENTION***\nOne of the auto incrementing fields in one of your database tables is about to run out of range of its datatype \nBelow is the details about that field:\n"
         msg = (
@@ -25,11 +26,12 @@ def notify(table_list):
 
         data = {"text": msg}
 
-    response = requests.post(
-        Constants.WEBHOOK, data=json.dumps(data), headers=headers, verify=False
-    )
-    if response.status_code != 200:
-        raise ValueError(
-            "Request to slack returned an error %s, the response is:\n%s"
-            % (response.status_code, response.text)
+        # Send message to slack using slack incoming webhook
+        response = requests.post(
+            Constants.WEBHOOK, data=json.dumps(data), headers=headers, verify=False
         )
+        if response.status_code != 200:
+            raise ValueError(
+                "Request to slack returned an error %s, the response is:\n%s"
+                % (response.status_code, response.text)
+            )
